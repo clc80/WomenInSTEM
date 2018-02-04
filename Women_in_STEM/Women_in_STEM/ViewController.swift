60ef8bc4d6b8524f90432c43a41212ead0c007ca
clc80 <clc_80@yahoo.com>
2018-02-03 19:42:50 -0800

    Shows a table view with the image name

---
+++ Women_in_STEM/Women_in_STEM/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Women_in_STEM
+//
+//  Created by Claudia Contreras on 2/3/18.
+//  Copyright © 2018 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //load images
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix(".jpg") {
+                //this is a picture to load
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+
+}
+
